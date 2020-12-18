13a6dd7692b8d255088cdb5cfc521820ec128363
Leo Nguyen <Leo.Nguyen@gmx.de>
2020-12-18 15:49:20 +0100

    implement clean up after downloading dataset + code tested and working

--- src/download_data.py
+++ src/download_data.py
@@ -1,5 +1,6 @@
 import os
 from os.path import exists, dirname, abspath, join
+import shutil
 import requests
 
 
@@ -7,6 +8,8 @@ import requests
 def download_TextComplexityDE19():
     """Download the TextComplexityDE dataset from Github Repository.
        Contact Badak Naderi (babak.naderi[at]tu-berlin.de) for further support concerning the dataset itself. 
+
+       Written by Leo Nguyen. Contact Xenovortex, if problems arises.
     """
     # url of dataset github repository
     url = "https://github.com/babaknaderi/TextComplexityDE.git"
@@ -19,12 +22,14 @@ def download_TextComplexityDE19():
     # check if data folder empty, if yes: download github repository to folder 
     if len(os.listdir(download_to_path)) == 0:
         os.system("git clone {} {}".format(url, download_to_path))
+        # clean up downloaded repository
+        shutil.rmtree(join(download_to_path, ".git"))
+        os.remove(join(download_to_path, ".gitignore"))
+        os.remove(join(download_to_path, "LICENSE"))
+        os.remove(join(download_to_path, "README.md"))
     else:
         print("Folder data is not empty. Please delete the folder data and remove from trash. Rerun the code.")
         exit()
-
-    # clean downloaded repository 
-
 
 
 if __name__ == "__main__":
