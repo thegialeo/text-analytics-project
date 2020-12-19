ba0fe49e18a6c3e8f17d2b6813f541424aa8316d
Leo Nguyen <Leo.Nguyen@gmx.de>
2020-12-19 18:19:52 +0100

    feature extraction from sentences with tfidf

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -1,8 +1,18 @@
 from os.path import dirname, abspath, join
 import pandas as pd
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 if __name__ == "__main__":
+    # read data
     data_path = join(dirname(dirname(abspath(__file__))), "data", "TextComplexityDE19")
-    ratings_df = pd.read_csv(join(data_path, "ratings.csv"), sep = ",", encoding = "ISO-8859-1")
-    print(ratings_df.head())
+    df_ratings = pd.read_csv(join(data_path, "ratings.csv"), sep = ",", encoding = "ISO-8859-1")
+    df_complexity = df_ratings[["Sentence", "MOS_Complexity"]]
+    
+    # feature extraction
+    german_stopwords = stopwords.words('german')
+    tfidf = TfidfVectorizer(encoding="ISO-8859-1", stop_words=german_stopwords)
+    tfidf.fit(df_complexity.Sentence.values)
+    features = tfidf.transform(df_complexity.Sentence.values)
+    
