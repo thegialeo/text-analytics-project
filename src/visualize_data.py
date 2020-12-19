52b792a0bf95b8cdb1aadb048aab571c39fcdd28
Leo Nguyen <Leo.Nguyen@gmx.de>
2020-12-19 20:24:37 +0100

    plot clustering result against targets (save plot to figures folder)

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -30,15 +30,22 @@ if __name__ == "__main__":
     reduced_features = pca.fit_transform(features.toarray())
     reduced_cluster_centers = pca.transform(cls_kmeans.cluster_centers_)
     
-    # Plot cluster
-    plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=cls_kmeans.predict(features))
-    plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:, 1], marker='x', s=150, c='r')
-    plt.show()
-
-    # Plot rounded targets
-    plt.scatter(reduced_features[:, 0], reduced_features[:, 1], c=df_complexity.MOS_Complexity.values.round(0))
-    plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:, 1], marker='x', s=150, c='r')
-    plt.show()
+    # Plot cluster result against targets and topic
+    fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(15,10))
+    ax[0].scatter(reduced_features[:, 0], reduced_features[:, 1], c=cls_kmeans.predict(features), alpha=0.5)
+    ax[0].scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:, 1], marker='x', s=100, c='r')
+    ax[1].scatter(reduced_features[:, 0], reduced_features[:, 1], c=df_complexity.MOS_Complexity.values.round(0), alpha=0.5)
+    ax[1].scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:, 1], marker='x', s=100, c='r')
+    ax[0].set_xlabel("feature 1")
+    ax[0].set_ylabel("feature 2")
+    ax[1].set_xlabel("feature 1")
+    ax[1].set_ylabel("feature 2")
+    ax[0].set_title("KMeans clustering result (Projection with PCA)")
+    ax[1].set_title("PCA features colorcoded by rounded MOS_Complexity")
+    ax[0].grid(True)
+    ax[1].grid(True)
+    plt.tight_layout()
+    fig.savefig(join(dirname(dirname(abspath(__file__))), "figures", "KMeans_clustering_MOS_Complexity"))
 
     # Evaluate homogeneity score
     print(homogeneity_score(df_complexity.MOS_Complexity.values.round(0), cls_kmeans.predict(features)))
