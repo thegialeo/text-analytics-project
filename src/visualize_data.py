e265b00cabc20d415f19d6028a1c683ced536da5
Leo Nguyen <Leo.Nguyen@gmx.de>
2020-12-19 18:34:21 +0100

    implement clustering with KMeans

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -1,13 +1,16 @@
 from os.path import dirname, abspath, join
 import pandas as pd
 from nltk.corpus import stopwords
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
     # read data
     data_path = join(dirname(dirname(abspath(__file__))), "data", "TextComplexityDE19")
-    df_ratings = pd.read_csv(join(data_path, "ratings.csv"), sep = ",", encoding = "ISO-8859-1")
+    df_ratings = pd.read_csv(join(data_path, "ratings.csv"), sep=",", encoding="ISO-8859-1")
     df_complexity = df_ratings[["Sentence", "MOS_Complexity"]]
     
     # feature extraction
@@ -15,4 +18,20 @@ if __name__ == "__main__":
     tfidf = TfidfVectorizer(encoding="ISO-8859-1", stop_words=german_stopwords)
     tfidf.fit(df_complexity.Sentence.values)
     features = tfidf.transform(df_complexity.Sentence.values)
+
+    # clustering with KMeans
+    cls_kmeans = MiniBatchKMeans(n_clusters=6, random_state=0)
+    cls_kmeans.fit(features)
+    cls_kmeans.predict(features)
+
+    
+    
+    
+    
+
+    
+    
+    
+
+    
     
