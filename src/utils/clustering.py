750798c9ce95108eb915e4054cf26cc522f5b431
Leo Nguyen <Leo.Nguyen@gmx.de>
2020-12-21 13:22:54 +0100

    rewrite visualize data to work with wrappers

--- src/utils/clustering.py
+++ src/utils/clustering.py
@@ -1,10 +1,8 @@
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.decomposition import PCA
-from sklearn.metrics import homogeneity_score, silhouette_score
-import matplotlib.pyplot as plt
 
 
-def clustering_wrapper(features, cluster_method='kmeans', dim_reduc='PCA', save_name=None):
+def clustering_wrapper(features, cluster_method='kmeans', dim_reduc='PCA'):
     """Performs clustering, dimension reduction to 2d space and plots the result (see folder figures).
        Evaluate homogeneity and silhouette score (see folder results).
 
@@ -12,21 +10,21 @@ def clustering_wrapper(features, cluster_method='kmeans', dim_reduc='PCA', save_
         features (array-like, sparse matrix): matrix with dimension (number samples, number features)
         cluster_method (str, optional): Select clustering method. Implemented so far are: 'kmeans'. Defaults to 'kmeans'.
         dim_reduc (str, optional): Select dimension reduction method. Implemented so far are: 'PCA'. Defaults to 'PCA'.
-        save_name (str, optional): name to save plots and result text file under. Defaults to None (plot and result will not be saved!).
 
     Return:
         sklearn_cls (sklearn.cluster class object): sklearn object (see documentation for sklearn.cluster)
         reduced_features (numpy array): features after applying dimension reduction
         reduced_cluster_centers (numpy array): cluster centers after applying dimension reduction
     """
-    
+
     # perform selected clustering method
     if cluster_method == 'kmeans':
         sklearn_cls = MiniBatchKMeans(n_clusters=6, random_state=0)
         sklearn_cls.fit(features)
         sklearn_cls.predict(features)
     else:
-        print("Clustering method {} is not implemented yet. Please select one of the following options: 'kmeans'".format(cluster_method))
+        print("Clustering method {} is not implemented yet. Please select one of the following options: 'kmeans'".format(
+            cluster_method))
         exit()
 
     # perform selected dimension reduction
--- src/visualize_data.py
+++ src/visualize_data.py
@@ -1,31 +1,26 @@
-from os.path import dirname, abspath, join
+from os.path import abspath, dirname, join
+
+import matplotlib.pyplot as plt
 import pandas as pd
 from nltk.corpus import stopwords
-from utils import vectorizer
-
+from sklearn.metrics import homogeneity_score, silhouette_score
+from utils import clustering, vectorizer
 
 if __name__ == "__main__":
     # read data
     data_path = join(dirname(dirname(abspath(__file__))), "data", "TextComplexityDE19")
     df_ratings = pd.read_csv(join(data_path, "ratings.csv"), sep=",", encoding="ISO-8859-1")
     df_complexity = df_ratings[["Sentence", "MOS_Complexity"]]
-    
+
     # feature extraction
     german_stopwords = stopwords.words('german')
     features = vectorizer.vectorizer_wrapper(df_complexity.Sentence.values, 'tfidf', german_stopwords)
 
-    # clustering with KMeans
-    cls_kmeans = MiniBatchKMeans(n_clusters=6, random_state=0)
-    cls_kmeans.fit(features)
-    cls_kmeans.predict(features)
+    # clustering with KMeans and dimension reduction with PCA
+    cls_kmeans, reduced_features, reduced_cluster_centers = clustering.clustering_wrapper(features, 'kmeans', 'PCA')
 
-    # Dimension reduction of feature space with PCA
-    pca = PCA(n_components=2, random_state=0)
-    reduced_features = pca.fit_transform(features.toarray())
-    reduced_cluster_centers = pca.transform(cls_kmeans.cluster_centers_)
-    
-    # Plot cluster result against targets 
-    fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(15,10))
+    # Plot cluster result against targets
+    fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(15, 10))
     ax[0].scatter(reduced_features[:, 0], reduced_features[:, 1], c=cls_kmeans.predict(features), alpha=0.5)
     ax[0].scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:, 1], marker='x', s=100, c='r')
     ax[1].scatter(reduced_features[:, 0], reduced_features[:, 1], c=df_complexity.MOS_Complexity.values.round(0), alpha=0.5)
@@ -45,4 +40,4 @@ if __name__ == "__main__":
     print(homogeneity_score(df_complexity.MOS_Complexity.values.round(0), cls_kmeans.predict(features)))
 
     # Evaluate silhouette score
-    print(silhouette_score(features, labels=cls_kmeans.predict(features)))   
+    print(silhouette_score(features, labels=cls_kmeans.predict(features)))
