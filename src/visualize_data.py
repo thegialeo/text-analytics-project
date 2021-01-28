8e711102b0741db9d02ae679fedd6240a185989a
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-01-28 19:27:04 +0100

    recover old visualize_data.py

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -3,88 +3,78 @@ from os.path import abspath, dirname, join
 import matplotlib.pyplot as plt
 import pandas as pd
 from nltk.corpus import stopwords
+from sklearn.metrics import homogeneity_score, silhouette_score
 from utils import clustering, vectorizer
-
 import to_dataframe
 
 
-def visualize_vectorizer(vec='tfdif', dim_reduc='PCA'):
-    """Apply vectorizer on TextComplexityDE19 data and visualize the vectorization.
 
-    Args:
-        vec (str, optional): vectorizer method to used (options: 'tfidf', 'count', 'hash'), default: 'tfidf'
-        dim_reduc (str, optional): dimension reduction method to used (options: 'PCA', 'TSNE'), default: 'PCA'
-    """
-    
-     # read data
-    data_path = join(dirname(dirname(dirname(abspath(__file__)))), "data", "TextComplexityDE19")
-    df_ratings = pd.read_csv(join(data_path, "ratings.csv"), sep=",", encoding="ISO-8859-1")
-
-
-
-def visualize_clustering(vec='tfidf', cluster='kmeans', dim_reduc='PCA'):
+def visualize_data():
     """Perform clustering, dimension reduction on TextComplexityDE19 data and plot the result.
+       Evaluate clustering by homogeneity and silhouette score.
 
        Written by Leo Nguyen. Contact Xenovortex, if problems arises.
-
-    Args:
-        vec (str, optional): vectorizer method to used (options: 'tfidf', 'count', 'hash'), default: 'tfidf'
-        cluster (str, optional): clustering method to used (options: 'kmeans', 'AP', 'mean_shift', 'spectral', 'Agg', 'DBSCAN', 'OPTICS', 'Birch'), default: 'kmeans'
-        dim_reduc (str, optional): dimension reduction method to used (options: 'PCA', 'TSNE'), default: 'PCA'
     """
 
-    # centroid methods
-    centroid_methods = ['kmeans', 'AP', 'mean_shift']
-
     # read data
-    data_path = join(dirname(dirname(dirname(abspath(__file__)))), "data", "TextComplexityDE19")
-    df_ratings = pd.read_csv(join(data_path, "ratings.csv"), sep=",", encoding="ISO-8859-1")
+    data_path = join(dirname(dirname(abspath(__file__))), "data", "TextComplexityDE19")
+    df_ratings = pd.read_csv(
+        join(data_path, "ratings.csv"),
+        sep=",", encoding="ISO-8859-1")
 
     # feature extraction
     german_stopwords = stopwords.words('german')
-    features = vectorizer.vectorizer_wrapper(df_ratings.Sentence.values, vec, german_stopwords)
+    features = vectorizer.vectorizer_wrapper(
+        df_ratings.Sentence.values, 'tfidf', german_stopwords)
     features = features.toarray()
 
-    # Clustering and Dimension Reduction
-    if cluster in centroid_methods:
-        cls_object, reduced_features, reduced_cluster_centers = clustering.clustering_wrapper(features, cluster, dim_reduc)
-    else: 
-        cls_object, reduced_features = clustering.clustering_wrapper(features, cluster, dim_reduc)
+    # KMeans Clustering and PCA
+    #cls_kmeans, reduced_features, reduced_cluster_centers = clustering.clustering_wrapper(
+     #   features, 'Birch', 'PCA')
+    cls_kmeans, reduced_features = clustering.clustering_wrapper(
+         features, 'Birch', 'PCA')
 
-    # Plotting
+    # Plot cluster result against targets
     fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(15, 10))
     ax[0].scatter(
         reduced_features[:, 0],
         reduced_features[:, 1],
-        c=cls_kmeans.labels_,
+        c=cls_kmeans.predict(features),
         alpha=0.5)
+    #ax[0].scatter(
+     #   reduced_cluster_centers[:, 0],
+      #  reduced_cluster_centers[:, 1],
+       # marker='x', s=100, c='r')
     ax[1].scatter(
         reduced_features[:, 0],
         reduced_features[:, 1],
         c=df_ratings.MOS_Complexity.values.round(0),
         alpha=0.5)
-
-    if cluster in centroid_methods:
-        ax[0].scatter(
-            reduced_cluster_centers[:, 0],
-            reduced_cluster_centers[:, 1],
-            marker='x', s=100, c='r')
-        ax[1].scatter(
-            reduced_cluster_centers[:, 0],
-            reduced_cluster_centers[:, 1],
-            marker='x', s=100, c='r')
-        
+    #ax[1].scatter(
+     #   reduced_cluster_centers[:, 0],
+      #  reduced_cluster_centers[:, 1],
+       # marker='x', s=100, c='r')
     ax[0].set_xlabel("feature 1")
     ax[0].set_ylabel("feature 2")
     ax[1].set_xlabel("feature 1")
     ax[1].set_ylabel("feature 2")
-    ax[0].set_title("{} clustering result (projection. {})".format(cluster, dim_reduc))
-    ax[1].set_title("rounded MOS_Complexity label")
+    ax[0].set_title("KMeans clustering result (Projection with PCA)")
+    ax[1].set_title("PCA features colorcoded by rounded MOS_Complexity")
     ax[0].grid(True)
     ax[1].grid(True)
     plt.tight_layout()
-    fig.savefig(join(dirname(dirname(dirname(abspath(__file__)))), "figures", "{}_{}_{}.png".format(cluster, vec, dim_reduc)))
-    
+    fig.savefig(
+        join(
+            dirname(dirname(abspath(__file__))),
+            "figures", "KMeans_clustering_MOS_Complexity"))
+    # Evaluate homogeneity score
+    print(
+        homogeneity_score(
+            df_ratings.MOS_Complexity.values.round(0),
+            cls_kmeans.labels_))
+            #cls_kmeans.predict(features)))
+    # Evaluate silhouette score
+    #print(silhouette_score(features, labels=cls_kmeans.predict(features)))
 
 def basic_stats():
 
@@ -135,4 +125,4 @@ def basic_stats():
 
 if __name__ == "__main__":
     visualize_data()
-    #basic_stats()
+    basic_stats()
