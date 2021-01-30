6cfbe3e29cb3e4f33a35f01330cfaf55d21d20fc
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-01-30 17:25:55 +0100

    add 2 custom stopword list

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,5 +1,7 @@
-import spacy
+from os.path import abspath, dirname, join
+
 import nltk
+import spacy
 import stop_words
 
 
@@ -7,7 +9,7 @@ def get_stopwords(source="nltk"):
     """Return German stopwords based on given source.
 
     Args:
-        source (str, optional): source to load stopwords from (options: "spacy", "nltk", "german_plain", "german_full"). Defaults to "nltk".
+        source (str, optional): source to load stopwords from (options: "spacy", "nltk", "stop_words", "german_plain", "german_full"). Defaults to "nltk".
 
     Return:
         stopwords (list): a list of german stopwords
@@ -18,8 +20,15 @@ def get_stopwords(source="nltk"):
     elif source == "spacy":
         spacy.load("de_core_news_sm", disable=["tagger", "parser","ner"])
         return list(spacy.lang.de.stop_words.STOP_WORDS)
-    elif source == "stop-words":
+    elif source == "stop_words":
         return stop_words.get_stop_words('de')
     elif source == "german_plain":
-        pass
+        path = join(dirname(dirname(dirname(abspath(__file__)))), "corpus", "stopwords", "german_stopwords_plain.txt")
+        return [line.rstrip('\n') for line in open(path)][9:]
+    elif source == "german_full":
+        path = join(dirname(dirname(dirname(abspath(__file__)))), "corpus", "stopwords", "german_stopwords_full.txt")
+        return [line.rstrip('\n') for line in open(path)][9:]
+    else:
+        print("stopword source {} is not implemented. Please select one the folling options: 'spacy', 'nltk', 'stop_words', 'german_plain', 'german_full'".format(source))
+        exit()
         
