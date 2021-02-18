c2f560ec7f9efc03de9fb787110cd6ce771f0131
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-02-18 17:12:28 +0100

    init BERT tokenizer and model

--- src/utils/BERT.py
+++ src/utils/BERT.py
@@ -1,12 +1,19 @@
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW, BertConfig
 
 
 class BERT:
     
-    def __init__(self):
+    def __init__(self, X_train, y_train, X_test, y_test, device):
         """BERT wrapper class
 
            Written by Leo Nguyen. Contact Xenovortex, if problems arises.
         """
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.device = device
 
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-german-cased', do_lower_case=True)
+
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-german-cased')
+        self.model = BertForSequenceClassification.from_pretrained('bert-base-german-cased', num_labels=6, output_attentions=False, output_hidden_states=False)
