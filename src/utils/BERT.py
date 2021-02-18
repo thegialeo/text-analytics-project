d08aa86db01e3507c80e04f62f3c2f81bb692ab1
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-02-18 17:25:47 +0100

    init AdemW optimizer

--- src/utils/BERT.py
+++ src/utils/BERT.py
@@ -1,9 +1,10 @@
-from transformers import BertTokenizer, BertForSequenceClassification, AdamW, BertConfig
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW, BertConfig, get_linear_schedule_with_warmup
+from utils import gpu
 
 
 class BERT:
     
-    def __init__(self, X_train, y_train, X_test, y_test, device):
+    def __init__(self, X_train, y_train, X_test, y_test):
         """BERT wrapper class
 
            Written by Leo Nguyen. Contact Xenovortex, if problems arises.
@@ -12,8 +13,15 @@ class BERT:
         self.y_train = y_train
         self.X_test = X_test
         self.y_test = y_test
-        self.device = device
+        self.lr = 1e-4
+        self.device = gpu.check_gpu()
 
-
+        # Load BERT tokenizer
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-german-cased')
+
+        # Load pretrained BERT and move to GPU (if available)
         self.model = BertForSequenceClassification.from_pretrained('bert-base-german-cased', num_labels=6, output_attentions=False, output_hidden_states=False)
+        self.model = self.model.to(self.device)
+
+        # init optimizer
+        self.optimizer = AdamW(self.model.parameters(), lr=self.lr, eps=1e-8)
