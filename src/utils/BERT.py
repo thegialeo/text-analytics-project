8eafe9f622653a3a352fb283f43915eedcf9a529
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-02-21 18:30:24 +0100

    Bert class: remove optimizer + scheduler

--- src/utils/BERT.py
+++ src/utils/BERT.py
@@ -1,33 +1,19 @@
-from transformers import BertTokenizer, BertForSequenceClassification, AdamW, BertConfig, get_linear_schedule_with_warmup
+from transformers import BertTokenizer, BertModel, AdamW, BertConfig, get_linear_schedule_with_warmup
 #from utils import gpu
 
 
 class BERT:
     
-    def __init__(self, sentences, labels, epoch, lr):
+    def __init__(self):
         """BERT wrapper class
 
            Written by Leo Nguyen. Contact Xenovortex, if problems arises.
         """
-        self.sentences = sentences
-        self.labels = labels
-        self.epoch = epoch
-        self.lr = lr
         self.device = gpu.check_gpu()
 
         # Load BERT tokenizer
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-german-cased', do_lower_case=True)
 
-        # feature extraction
-        self.features = self.tokenizer.encode(self.sentences)
-
         # Load pretrained BERT and move to GPU (if available)
         self.model = BertForSequenceClassification.from_pretrained('bert-base-german-cased', num_labels=6, output_attentions=False, output_hidden_states=False)
         self.model = self.model.to(self.device)
-
-        # init optimizer
-        self.optimizer = AdamW(self.model.parameters(), lr=self.lr, eps=1e-8)
-
-        # init scheduler
-        self.scheduler=get_linear_schedule_with_warmup(self.optimizer, num_warmup_steps=0, num_training_steps=len(trainloader) * self.epoch)
-        
