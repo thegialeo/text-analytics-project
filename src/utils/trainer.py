7050783e1687000e1097905db58688713be2cb58
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-02-21 19:56:16 +0100

    restructure arguments for train_model function

--- src/utils/trainer.py
+++ src/utils/trainer.py
@@ -1,15 +1,15 @@
 
 
-def train_model(model, optimizer, criterion, num_epoch, trainloader, testloader, save_name):
-    """[summary]
+def train_model(filename, model, optimizer, criterion, num_epoch, save_name, use_BERT=False):
+    """Train a model on the given dataset
 
     Args:
+        filename (string): name of h5 file containing dataset
         model (PyTorch nn.Module): PyTorch neural network architecture
         optimizer (PyTorch optimizer): optimization method
         criterion (function): loss function
         num_epoch (int): number of epochs
-        trainloader (PyTorch dataloader): dataloader for trainset
-        testloader (PyTorch dataloader): dataloader for testset
         save_name (string): name under which to save trained model and results
+        use_BERT (bool): use pretrained BERT for feature extraction
     """
     pass
