9805bfa3499ff386f4336c85b9f1963b969ceb9b
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-02-21 19:53:01 +0100

    documentation for function to train neural network

---
+++ src/utils/trainer.py
@@ -0,0 +1,15 @@
+
+
+def train_model(model, optimizer, criterion, num_epoch, trainloader, testloader, save_name):
+    """[summary]
+
+    Args:
+        model (PyTorch nn.Module): PyTorch neural network architecture
+        optimizer (PyTorch optimizer): optimization method
+        criterion (function): loss function
+        num_epoch (int): number of epochs
+        trainloader (PyTorch dataloader): dataloader for trainset
+        testloader (PyTorch dataloader): dataloader for testset
+        save_name (string): name under which to save trained model and results
+    """
+    pass
