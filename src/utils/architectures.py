a2643396fff84ddc15971e4b7c3dddf751eaca8b
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-03-10 15:42:59 +0100

    add network architecture with dropout

--- src/utils/architectures.py
+++ src/utils/architectures.py
@@ -28,7 +28,7 @@ class BN_Net(nn.Module):
     """
 
     def __init__(self, num_features, num_hidden, num_output):
-        super(BN_Dropout_Net, self).__init__()
+        super(BN_Net, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(num_features, num_hidden),
             nn.BatchNorm1d(num_hidden),
@@ -41,3 +41,27 @@ class BN_Net(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+class Dropout_Net(nn.Module):
+    """3-layer Neural Network with Dropout
+
+    Written by Leo Nguyen. Contact Xenovortex, if problems arises.
+    """
+
+    def __init__(self, num_features, num_hidden, num_output):
+        super(Dropout_Net, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(num_features, num_hidden),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+            nn.Linear(num_hidden, num_hidden),
+            nn.ReLU(True), 
+            nn.Dropout(0.2),
+            nn.Linear(num_hidden, num_output)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
