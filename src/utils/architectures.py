1ca1da48a4be1fd0a63d549088cc0330fe9ff647
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-03-10 15:56:24 +0100

    add network architecture with both dropout and batch normalization

--- src/utils/architectures.py
+++ src/utils/architectures.py
@@ -54,7 +54,7 @@ class Dropout_Net(nn.Module):
         self.model = nn.Sequential(
             nn.Linear(num_features, num_hidden),
             nn.ReLU(True),
-            nn.Dropout(0.5),
+            nn.Dropout(0.5), 
             nn.Linear(num_hidden, num_hidden),
             nn.ReLU(True), 
             nn.Dropout(0.2),
@@ -65,3 +65,22 @@ class Dropout_Net(nn.Module):
         return self.model(x)
 
 
+class BN_Dropout_Net(nn.Module):
+    """3-layer Neural Network with Dropout and Batch Normalization
+
+    Written by Leo Nguyen. Contact Xenovortex, if problems arises.
+    """
+
+    def __init__(self, num_features, num_hidden, num_output):
+        super(BN_Dropout_Net, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(num_features, num_hidden),
+            nn.BatchNorm1d(num_hidden),
+            nn.ReLU(True),
+            nn.Dropout(0.5),
+            nn.Linear(num_hidden, num_hidden),
+            nn.BatchNorm1d(num_hidden),
+            nn.ReLU(True),
+            nn.Dropout(0.2),
+            nn.Linear(num_hidden, num_output)
+        )
