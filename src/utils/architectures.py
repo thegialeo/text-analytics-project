1469476328bf6e954d7056f36ca58fdad8bdf4eb
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-03-10 15:09:45 +0100

    add network archtitecture with Batch Normalization

--- src/utils/architectures.py
+++ src/utils/architectures.py
@@ -21,4 +21,23 @@ class Net(nn.Module):
         return self.model(x)
 
 
+class BN_Net(nn.Module):
+    """3-layer Neural Network with Batch Normalization
 
+    Written by Leo Nguyen. Contact Xenovortex, if problems arises.
+    """
+
+    def __init__(self, num_features, num_hidden, num_output):
+        super(BN_Dropout_Net, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(num_features, num_hidden),
+            nn.BatchNorm1d(num_hidden),
+            nn.ReLU(True),
+            nn.Linear(num_hidden, num_hidden),
+            nn.BatchNorm1d(num_hidden),
+            nn.ReLU(True),
+            nn.Linear(num_hidden, num_output)
+        )
+
+    def forward(self, x):
+        return self.model(x)
